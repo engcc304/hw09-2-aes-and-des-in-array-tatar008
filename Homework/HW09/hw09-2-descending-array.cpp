da1f01c6efc9cf8d7c8d4122884961823361afaa
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-09-20 18:10:47 +0700

    hw09-2

--- Homework/HW09/hw09-2-descending-array.cpp
+++ Homework/HW09/hw09-2-descending-array.cpp
@@ -23,3 +23,40 @@
         99-0 : 13 9 6 5 4 1 1
 
 */
+#include<stdio.h>
+int main() {
+    int arr[100];
+    int i = 0 ;
+    int j = 0 ;
+    while (1) {
+        printf( "Input :\n" ) ;
+        scanf( "%d", &arr[i] ) ;
+        if(arr[i] == -1 ) {
+            break;
+        }
+        i++ ;
+    }
+
+    for (int j = 0; j < i - 1; j++) {
+        for (int k = 0; k < i - j - 1; k++) {
+            if (arr[k] > arr[k + 1]) {
+                // สลับค่า
+                int temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+        }
+    }
+    
+    printf( "0-99 : ") ;
+    for(j = 0; j < i ; j++ ) {
+        printf( "%d ", arr[j] ) ;
+    }
+    printf("\n99-0 : ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d ", arr[j]);
+    }
+    
+    return 0;
+
+}
